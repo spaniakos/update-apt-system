b9357fbaeb2c8f6782994fb8df063807b38edf70
spaniakos <spaniakos@gmail.com>
2023-10-16 18:51:51 +0300

    Docstrings and typos

--- update.sh
+++ update.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 # ------------------------------------------------------------------
-# [Spaniakos] Update script
-#          The script updates the system.
+# [SCRIPT_NAME] update.sh
+# [VERSION] 1.1.2
+# [AUTHOR] G.Spanos - gspanos@makeasite.gr - spaniakos@gmail.com - spaniakos - drvs
+# [CREATE_DATE] 2/9/2016 [DD/MM/YYYY]
+# [LAST_UPDATE] 16/10/2023 [DD/MM/YYYY]
 # ------------------------------------------------------------------
+# This script updates the apt system and various manual packages and commands.
+# It can be used to update the system without asking for confirmation or by asking for confirmation.
+# ------------------------------------------------------------------
+
 SCRIPT_NAME="$0"
 VERSION="1.1.1"
-AUTHOR="G.Spanos - gspanos@makeasite.gr"
+AUTHOR="G.Spanos - gspanos@makeasite.gr - spaniakos@gmail.com - spaniakos - drvs"
 CREATE_DATE="2/9/2016"
 LAST_UPDATE="16/10/2023"
 
 ROOT_UID=0	# Only users with $UID 0 have root privileges.
 E_NOTROOT=87	# Non-root exit error.
-E_DONE=0	# Normal exit. 
-# Run as root, of course.
+E_DONE=0	# Normal exit. No errors.
 
+# This function runs a command as a specified user.
 # $1 : Username
 # $2 : Command `command`
 function run_as_user() {
@@ -23,6 +30,8 @@ function run_as_user() {
 EOF
 }
 
+# This function checks if the user running the script is the root user.
+# If not, it calls the help function.
 function RU_ROOT {
 	if [ "$UID" -ne "$ROOT_UID" ]
 	then
@@ -30,8 +39,13 @@ function RU_ROOT {
 	fi
 }
 
-#function to check if a package is installed and return true or false
-# $1 : package name
+# Function to check if a package is installed and return true or false
+# 
+# Arguments:
+#   $1: package name
+#
+# Returns:
+#   0 if package is installed, 1 otherwise
 function is_package_installed {
 	if dpkg -s $1 >/dev/null 2>&1; then
 		return 0;
@@ -40,8 +54,13 @@ function is_package_installed {
 	fi
 }
 
-#function to check if a command is installed and return true or false
-# $1 : command name
+# Function to check if a command is installed and return true or false
+# 
+# Arguments:
+#   $1: command name
+#
+# Returns:
+#   0 if command is installed, 1 otherwise
 function is_command_installed {
 	if command -v $1 >/dev/null 2>&1; then
 		return 0;
@@ -50,152 +69,208 @@ function is_command_installed {
 	fi
 }
 
+# This function updates the system without asking for confirmation by updating the repositories, upgrading the installed programs, 
+# upgrading the system, cleaning up the system, and updating additional packages such as flatpak, snap, 
+# rust, composer, and freshclam (clamAV) if they are installed.
+# It also prints the start and end time of the update process.
+# Parameters:
+# $1 - The user to run rustup update as (if rustup is installed)
 function Update_Without_Asking {
 	current_date_time=$(date +"%Y-%m-%d %H:%M:%S")
 	echo -e "\e[1m\e[92m[Update starting]: $current_date_time\e[21m\e[39m";
+	
+	# Update repositories
 	echo -e "\e[1m\e[91m[Updating Repos]\e[21m\e[39m";
 	apt-get update;
 	echo -e "\e[1m\e[92m[DONE]\e[21m\e[39m";
 	echo -e "";
+	
+	# Upgrade installed programs
 	echo -e "\e[1m\e[91m[Updating programms]\e[21m\e[39m";
 	apt-get upgrade -y;
 	echo -e "\e[1m\e[92m[DONE]\e[21m\e[39m";
 	echo -e "";
+	
+	# Upgrade the system
 	echo -e "\e[1m\e[91m[Updating system]\e[21m\e[39m";
 	apt-get dist-upgrade -y;
 	echo -e "\e[1m\e[92m[DONE]\e[21m\e[39m";
 	echo -e "";
+	
+	# Clean up the system
 	echo -e "\e[1m\e[91m[Cleaning up]\e[21m\e[39m";
 	apt-get autoremove -y;
 	apt-get autoclean -y;
 	echo -e "\e[1m\e[92m[DONE]\e[21m\e[39m";
 	echo -e "";
-	#check if flatpak is installed
+	
+	# Check if flatpak is installed and update it if available
 	if is_package_installed flatpak; then
 		echo -e "\e[1m\e[91m[Updating flatpak If available]\e[21m\e[39m";
 		flatpak update -y;
 		echo -e "\e[1m\e[92m[DONE]\e[21m\e[39m";
 		echo -e "";
 	fi
-	#check if snap is installed
+	
+	# Check if snap is installed and update it if available
 	if is_package_installed snap; then
 		echo -e "\e[1m\e[91m[Updating snap]\e[21m\e[39m";
 		snap refresh;
 		echo -e "\e[1m\e[92m[DONE]\e[21m\e[39m";
 		echo -e "";
 	fi
-	#check if rust command is installed
+	
+	# Check if rust command is installed and update it if available
 	if is_command_installed rustup; then
 		echo -e "\e[1m\e[91m[Updating rust If available]\e[21m\e[39m";
 		run_as_user $1 "rustup update"
 		echo -e "\e[1m\e[92m[DONE]\e[21m\e[39m";
 		echo -e "";
 	fi
-	#check if composer command is installed
+	
+	# Check if composer command is installed and update it if available
 	if is_command_installed composer; then
 		echo -e "\e[1m\e[91m[Updating Composer If available]\e[21m\e[39m";
 		composer self-update
 		echo -e "\e[1m\e[92m[DONE]\e[21m\e[39m";
 		echo -e "";
 	fi
-	#check if freshclam command is installed
+	
+	# Check if freshclam command is installed and update it if available
 	if is_command_installed freshclam; then
 		echo -e "\e[1m\e[91m[Updating freshclam (clamAV) If available]\e[21m\e[39m";
 		freshclam
 		echo -e "\e[1m\e[92m[DONE]\e[21m\e[39m";
 		echo -e "";
 	fi
+	
 	end_date_time=$(date +"%Y-%m-%d %H:%M:%S")
 	echo -e "\e[1m\e[92m[Update finished]: $end_date_time\e[21m\e[39m";
 	exit $E_DONE;
 }
 
+# This function updates the system by updating the repositories, upgrading the installed programs, 
+# upgrading the system, cleaning up the system, and updating additional packages such as flatpak, snap, 
+# rust, composer, and freshclam (clamAV) if they are installed.
+# It also prints the start and end time of the update process.
+# Parameters:
+# $1 - The user to run rustup update as (if rustup is installed)
 function Update_With_Asking {
 	current_date_time=$(date +"%Y-%m-%d %H:%M:%S")
 	echo -e "\e[1m\e[92m[Update starting]: $current_date_time\e[21m\e[39m";
+	
+	# Update repositories
 	echo -e "\e[1m\e[91m[Updating Repos]\e[21m\e[39m";
 	apt-get update;
 	echo -e "\e[1m\e[92m[DONE]\e[21m\e[39m";
 	echo -e "";
+	
+	# Upgrade installed programs
 	echo -e "\e[1m\e[91m[Updating programms]\e[21m\e[39m";
 	apt-get upgrade;
 	echo -e "\e[1m\e[92m[DONE]\e[21m\e[39m";
 	echo -e "";
+	
+	# Upgrade the system
 	echo -e "\e[1m\e[91m[Updating system]\e[21m\e[39m";
 	apt-get dist-upgrade;
 	echo -e "\e[1m\e[92m[DONE]\e[21m\e[39m";
 	echo -e "";
+	
+	# Clean up the system
 	echo -e "\e[1m\e[91m[Cleaning up]\e[21m\e[39m";
 	apt-get autoremove;
 	apt-get autoclean;
 	echo -e "\e[1m\e[92m[DONE]\e[21m\e[39m";
 	echo -e "";
-	#check if flatpak is installed
+	
+	# Check if flatpak is installed and update it if available
 	if is_package_installed flatpak; then
 		echo -e "\e[1m\e[91m[Updating flatpak If available]\e[21m\e[39m";
 		flatpak update;
 		echo -e "\e[1m\e[92m[DONE]\e[21m\e[39m";
 		echo -e "";
 	fi
-	#check if snap is installed
+	
+	# Check if snap is installed and update it if available
 	if is_package_installed snap; then
-		echo -e "\e[1m\e[91m[Updating snap If available]\e[21m\e[39m";
+		echo -e "\e[1m\e[91m[Updating snap]\e[21m\e[39m";
 		snap refresh;
 		echo -e "\e[1m\e[92m[DONE]\e[21m\e[39m";
 		echo -e "";
 	fi
-	#check if rust command is installed
+	
+	# Check if rust command is installed and update it if available
 	if is_command_installed rustup; then
 		echo -e "\e[1m\e[91m[Updating rust If available]\e[21m\e[39m";
 		run_as_user $1 "rustup update"
 		echo -e "\e[1m\e[92m[DONE]\e[21m\e[39m";
 		echo -e "";
 	fi
-	#check if composer command is installed
+	
+	# Check if composer command is installed and update it if available
 	if is_command_installed composer; then
 		echo -e "\e[1m\e[91m[Updating Composer If available]\e[21m\e[39m";
 		composer self-update
 		echo -e "\e[1m\e[92m[DONE]\e[21m\e[39m";
 		echo -e "";
 	fi
-	#check if freshclam command is installed
+	
+	# Check if freshclam command is installed and update it if available
 	if is_command_installed freshclam; then
 		echo -e "\e[1m\e[91m[Updating freshclam (clamAV) If available]\e[21m\e[39m";
 		freshclam
 		echo -e "\e[1m\e[92m[DONE]\e[21m\e[39m";
 		echo -e "";
 	fi
+	
 	end_date_time=$(date +"%Y-%m-%d %H:%M:%S")
 	echo -e "\e[1m\e[92m[Update finished]: $end_date_time\e[21m\e[39m";
 	exit $E_DONE;
 }
 
+# Function to display help message
 function display_help {
-	echo -e "This is a programm to update your system."
+	# Display program description
+	echo -e "This is a program to update your system."
 	echo -e "";
-	echo -e "The programm requires root permitions."
+
+	# Display program requirements
+	echo -e "The program requires root permissions."
 	echo -e "";
+
+	# Display program usage
 	echo -e "$SCRIPT_NAME -[v|h|y|n|u]"
 	echo -e "";
+
+	# Display plain parameters
 	echo -e "Plain parameters:";
 	echo -e "\t -v \t\t outputs version."
-	echo -e "\t -h \t\t play this message"
+	echo -e "\t -h \t\t display this message"
 	echo -e "\t -y \t\t do not ask for updates, this is the default usage action"
 	echo -e "\t -n \t\t ask for updates"
 	echo -e "";
+
+	# Display argument parameters
 	echo -e "Argument parameters:";
 	echo -e "\t -u <user> \t Specific user name to run some commands like rustup"
 	echo -e "";
+
+	# Display double quotes variables
 	echo -e "Double quotes variables"
 	echo -e "\t --version \t outputs version."
-	echo -e "\t --help \t play this message"
-	}
+	echo -e "\t --help \t display this message"
+}
 
+# This function displays help information and exits the script with a non-zero exit code.
 function help {
 	display_help
 	exit $E_NOTROOT
 }
 
+# This function displays the version information of the script.
+# It includes the script name, version number, author, and last update date.
+# It also exits the script with a non-zero status code.
 function version {
 	echo -e "$SCRIPT_NAME";
 	echo -e "\tVERSION: $VERSION";
